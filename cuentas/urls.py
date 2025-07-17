a76ee4a70f27e1a6a7dae6585cd7880a0c7b13f5
Santiago Nervi <santii58.sn@gmail.com>
2025-07-17 08:14:19 -0300

    error al cambiar contraseña corregido(cambie de nombre a mi template password_change_done a password_change_complete)

--- cuentas/urls.py
+++ cuentas/urls.py
@@ -9,12 +9,12 @@ urlpatterns = [
     path('logout/', auth_views.LogoutView.as_view(), name='logout'),
 
     path('password_change/', auth_views.PasswordChangeView.as_view(
-         template_name='registration/password_change_form.html'),
+         template_name='registration/password_change.html'),
          name='password_change'),
 
     path('password_change/done/', auth_views.PasswordChangeDoneView.as_view(
-         template_name='registration/password_change_done.html'),
-         name='password_change_done'),
+        template_name='registration/password_change_complete.html'),
+        name='password_change_done'),
 
     path('perfil/', views.PerfilDetalle.as_view(), name='perfil'),
     path('perfil/editar/', views.PerfilEditar.as_view(), name='editar_perfil'),
