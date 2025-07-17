d96f025f833e91d1f71adf1a96b3bce19ca6f495
Santiago Nervi <santii58.sn@gmail.com>
2025-07-17 05:11:22 -0300

    agrego las views de la app cuentas

---
+++ cuentas/urls.py
@@ -0,0 +1,21 @@
+from django.urls import path
+from django.contrib.auth import views as auth_views
+from . import views
+
+urlpatterns = [
+    path('registro/', views.registro, name='registro'),
+    path('login/', auth_views.LoginView.as_view(
+         template_name='registration/login.html'), name='login'),
+    path('logout/', auth_views.LogoutView.as_view(), name='logout'),
+
+    path('password_change/', auth_views.PasswordChangeView.as_view(
+         template_name='registration/password_change_form.html'),
+         name='password_change'),
+
+    path('password_change/done/', auth_views.PasswordChangeDoneView.as_view(
+         template_name='registration/password_change_done.html'),
+         name='password_change_done'),
+
+    path('perfil/', views.PerfilDetalle.as_view(), name='perfil'),
+    path('perfil/editar/', views.PerfilEditar.as_view(), name='editar_perfil'),
+]
--- cuentas/views.py
+++ cuentas/views.py
@@ -1,3 +1,33 @@
-from django.shortcuts import render
+from django.shortcuts import render, redirect
+from django.views.generic import DetailView, UpdateView
+from django.urls import reverse_lazy
+from django.contrib.auth.mixins import LoginRequiredMixin
+from .forms import RegistroForm, PerfilForm
+from django.contrib.auth.models import User
 
-# Create your views here.
+def registro(request):
+    if request.method == 'POST':
+        form = RegistroForm(request.POST)
+        if form.is_valid():
+            form.save()
+            return redirect('login')
+    else:
+        form = RegistroForm()
+    return render(request, 'registration/registro.html', {'form': form})
+
+class PerfilDetalle(LoginRequiredMixin, DetailView):
+    model = User
+    template_name = 'registration/perfil.html'
+    context_object_name = 'usuario'
+
+    def get_object(self):
+        return self.request.user
+
+class PerfilEditar(LoginRequiredMixin, UpdateView):
+    model       = User
+    form_class  = PerfilForm
+    template_name = 'registration/editar_perfil.html'
+    success_url = reverse_lazy('perfil')
+
+    def get_object(self):
+        return self.request.user.perfil
